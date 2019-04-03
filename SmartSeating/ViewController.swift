8caaae7073463b70876025ae92ab99857b58042d
Dylan Maryk <dylan@door2door.io>
2019-04-03 17:31:48 +0200

    Add button images and other UI stuff

--- SmartSeating/ViewController.swift
+++ SmartSeating/ViewController.swift
@@ -10,12 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet private weak var dedicatedSeatButton: UIButton!
+    @IBOutlet private weak var regularSeatButton: UIButton!
     @IBOutlet private weak var networkAreaButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        dedicatedSeatButton.layer.cornerRadius = dedicatedSeatButton.frame.height / 2
+        regularSeatButton.layer.cornerRadius = regularSeatButton.frame.height / 2
         networkAreaButton.layer.cornerRadius = networkAreaButton.frame.height / 2
     }
     
