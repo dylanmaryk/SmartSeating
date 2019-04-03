3584fee41c60db26ff93b0df6c308852eaf479c1
Dylan Maryk <dylan@door2door.io>
2019-04-03 21:34:50 +0200

    Set labels from interests response

--- SmartSeating/YourSeatViewController.swift
+++ SmartSeating/YourSeatViewController.swift
@@ -11,11 +11,37 @@ import UIKit
 
 class YourSeatViewController: UIViewController {
     
+    @IBOutlet private weak var yourSeatLabel: UILabel!
+    @IBOutlet private weak var neighbor1Label: UILabel!
+    @IBOutlet private weak var neighbor2Label: UILabel!
+    @IBOutlet private weak var neighbor1Interest1Label: UILabel!
+    @IBOutlet private weak var neighbor1Interest2Label: UILabel!
+    @IBOutlet private weak var neighbor1Interest3Label: UILabel!
+    @IBOutlet private weak var neighbor1Interest4Label: UILabel!
+    @IBOutlet private weak var neighbor1Interest5Label: UILabel!
+    @IBOutlet private weak var neighbor2Interest1Label: UILabel!
+    @IBOutlet private weak var neighbor2Interest2Label: UILabel!
+    @IBOutlet private weak var neighbor2Interest3Label: UILabel!
+    @IBOutlet private weak var neighbor2Interest4Label: UILabel!
+    @IBOutlet private weak var neighbor2Interest5Label: UILabel!
+    
     @IBOutlet private weak var getYourTicketButton: UIButton!
     
     var interestsResponse: InterestsResponse? {
         didSet {
-            
+            yourSeatLabel.text = interestsResponse?.seat
+            neighbor1Label.text = interestsResponse?.neighbors[0].assignedSeat ?? ""
+            neighbor2Label.text = interestsResponse?.neighbors[1].assignedSeat ?? ""
+            neighbor1Interest1Label.text = interestsResponse?.neighbors[0].interests[0] ?? ""
+            neighbor1Interest2Label.text = interestsResponse?.neighbors[0].interests[1] ?? ""
+            neighbor1Interest3Label.text = interestsResponse?.neighbors[0].interests[2] ?? ""
+            neighbor1Interest4Label.text = interestsResponse?.neighbors[0].interests[3] ?? ""
+            neighbor1Interest5Label.text = interestsResponse?.neighbors[0].interests[4] ?? ""
+            neighbor2Interest1Label.text = interestsResponse?.neighbors[1].interests[0] ?? ""
+            neighbor2Interest2Label.text = interestsResponse?.neighbors[1].interests[1] ?? ""
+            neighbor2Interest3Label.text = interestsResponse?.neighbors[1].interests[2] ?? ""
+            neighbor2Interest4Label.text = interestsResponse?.neighbors[1].interests[3] ?? ""
+            neighbor2Interest5Label.text = interestsResponse?.neighbors[1].interests[4] ?? ""
         }
     }
     
